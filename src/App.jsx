a141cf58eddda487f3d94a78270371ffb8d6319d
gdrmvrm <gdrmvrm@gmail.com>
2025-12-21 19:07:18 +0000

    functionality for limit selector

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import CoinCard from './components/CoinCard';
+import LimitSelector from './components/LimitSelector';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -7,31 +8,34 @@ const App = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [limit, setLimit] = useState(10);
 
   useEffect(() => {
-    fetchCoins();
-  }, []);
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(
+          `${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`
+        );
+        if (!res.ok) throw new Error('Failed to fetch data');
+        const data = await res.json();
+        setCoins(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  const fetchCoins = async () => {
-    try {
-      const res = await fetch(
-        `${API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`
-      );
-      if (!res.ok) throw new Error('Failed to fetch data');
-      const data = await res.json();
-      setCoins(data);
-    } catch (err) {
-      setError(err.message);
-    } finally {
-      setLoading(false);
-    }
-  };
+    fetchCoins();
+  }, [limit]);
 
   return (
     <div>
       <h1>🚀 Crypto Dash</h1>
       {loading && <p>Loading...</p>}
       {error && <div className='error'>{error}</div>}
+
+      <LimitSelector limit={limit} onLimitChange={setLimit} />
 
       {!loading && !error && (
         <main className='grid'>
