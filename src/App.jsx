b1497a3f95da3c84114ea6701607a38562af8c93
gdrmvrm <gdrmvrm@gmail.com>
2025-12-21 18:49:29 +0000

    fetch coins

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import CoinCard from './components/CoinCard';
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const App = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    fetchCoins();
+  }, []);
+
+  const fetchCoins = async () => {
+    try {
+      const res = await fetch(
+        `${API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`
+      );
+      if (!res.ok) throw new Error('Failed to fetch data');
+      const data = await res.json();
+      setCoins(data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <h1>🚀 Crypto Dash</h1>
+      {loading && <p>Loading...</p>}
+      {error && <div className='error'>{error}</div>}
+
+      {!loading && !error && (
+        <main className='grid'>
+          {coins.map((coin) => (
+            <CoinCard key={coin.id} coin={coin} />
+          ))}
+        </main>
+      )}
+    </div>
+  );
+};
+
+export default App;
